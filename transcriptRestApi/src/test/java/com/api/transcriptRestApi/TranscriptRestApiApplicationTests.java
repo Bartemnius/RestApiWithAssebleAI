994594f1cc5e05d264fc2312127c0e3f37e936d1
Bartemnius <wojtekdluski@wp.pl>
2023-09-27 17:21:58 +0200

    First commit to the project. Just Hello working

---
+++ transcriptRestApi/src/test/java/com/api/transcriptRestApi/TranscriptRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.transcriptRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
